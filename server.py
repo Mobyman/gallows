fa60a3b41c8ea8232268c8e6f3b997d8980bd219
eixezi@gmail.com <eixezi@gmail.com@1a76497d-3899-4076-648e-19375577b5d5>
2011-03-05 14:37:44 +0000

    Eclipse IDE

--- server.py
+++ server.py
@@ -5,9 +5,26 @@ print "Hello!\n"
 
 HOST = getfqdn(gethostname())
 PORT = 14880
+fullServerFlag = False
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(HOST, PORT)
-server.listen(1)
-
+class Server:
+  
+  def listen():
+    
+    try:
+      server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      server.bind((HOST, PORT))
+      server.listen(1)
+      
+    except socket.error, detail: print detail
+      
+    while (fullServerFlag != True):
+      users = [server.fileno()]  
+      conn, addr = server.accept()
+      try: iw, ow, er = select.select(users, [], [], 3)
+      except: print detail
+      
+      print "%s has been connected!" % addr
+        
+    conn.close()
 
