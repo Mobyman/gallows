ee7131a3480b20d79918296ac112791f1a2f13ae
eixezi@gmail.com <eixezi@gmail.com@1a76497d-3899-4076-648e-19375577b5d5>
2011-03-05 19:22:20 +0000

    С горем устанавливает соединение

--- server.py
+++ server.py
@@ -1,30 +1,57 @@
-import socket, string, threading, sys
+# -*- coding: utf-8 -*-
+import socket, string, threading, sys, select
 global HOST, PORT
 
-print "Hello!\n"
 
-HOST = getfqdn(gethostname())
-PORT = 14880
-fullServerFlag = False
 
 class Server:
   
-  def listen():
-    
-    try:
-      server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-      server.bind((HOST, PORT))
-      server.listen(1)
-      
-    except socket.error, detail: print detail
-      
-    while (fullServerFlag != True):
-      users = [server.fileno()]  
-      conn, addr = server.accept()
-      try: iw, ow, er = select.select(users, [], [], 3)
-      except: print detail
-      
-      print "%s has been connected!" % addr
-        
-    conn.close()
 
+    
+  def run(self):
+
+    uobj = [] 
+    uready = []
+    running = 1
+    gamers = {} # addr, attempt
+     
+    while running:
+      nusers = [self.server]  
+      try: iw, ow, ew = select.select(nusers, [], [])
+      except select: print "Error select"
+      
+      for num in iw:
+        if num == self.server():
+          conn, addr = self.server.accept()
+          #self.newconn(str(num))
+          gamers[conn] = {addr, 10}
+          buf = num.recv(1024)
+          print buf
+          break
+        else:
+          data = num.recv(1024)
+          if data:
+            print data
+            #conn.close()
+          else:
+            break
+          
+
+  def __init__(self):
+    print "Hello!\n"
+    HOST = "localhost" #gethostbyaddr(gethostname())
+    PORT = 14880
+    try:
+      self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      self.server.bind((HOST, PORT))
+      self.server.listen(1)
+    except socket.error, detail: print detail
+    self.run()
+    
+  def newconn(self, user):
+    print "%s has been connected!" % user
+    # Добавить!
+
+server = Server()
+
+              
